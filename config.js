f1398b721f66dfc8584f194210d5fde04630ac15
Harish.K <harish2704@gmail.com>
2014-09-15 18:24:50 +0530

    fix for openshift

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ var config = {
   production: {
     root: rootPath,
     port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
-    db: process.env.DB_URL || 'mongodb://localhost/scheduler'
+    db: process.env.DB_URL || process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/scheduler'
   }
 };
 
